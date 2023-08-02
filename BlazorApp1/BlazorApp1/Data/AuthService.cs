a1c404ea71bacefbc20000b77a8418dcc1cf8e33
grimrapster <grimrapster668@gmail.com>
2023-08-02 00:24:48 -0500

    Initial commit

---
+++ BlazorApp1/BlazorApp1/Data/AuthService.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Threading.Tasks;
+
+public class AuthService
+{
+    public async Task<bool> AuthenticateAsync(string username, string password)
+    {
+        // This is a simple example that always returns true
+        // In a real application, you would perform actual authentication logic here.
+        return true;
+    }
+}
